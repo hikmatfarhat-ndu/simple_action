dda2b3e1fb6e6b399e8237548ad211074551c817
hikmat farhat <hfarhat@ndu.edu.lb>
2021-02-09 05:01:07 +0200

    update

--- helper.ts
+++ helper.ts
@@ -1,21 +1,21 @@
 import * as core from '@actions/core'
 import * as github from '@actions/github'
 
-  export const run=(input:string) =>{
-  const token = process.env['GITHUB_TOKEN'] || core.getInput('token')
-  if (!token || token === ''){
-      console.log(`token UNDEFINED`)
-  }
-  else{
-      console.log(`token DEFINED`)
-  }
+  export const run=function (input: string) {
+      const token = process.env['GITHUB_TOKEN'] || core.getInput('token')
+      if (!token || token === '') {
+          console.log(`token UNDEFINED`)
+      }
+      else {
+          console.log(`token DEFINED`)
+      }
 
-  // Create the octokit client
-  const octokit: github.GitHub = new github.GitHub(token)
-  if (!octokit){
-      console.log('CANNOT octokit')
+      // Create the octokit client
+      const octokit: github.GitHub = new github.GitHub(token)
+      if (!octokit) {
+          console.log('CANNOT octokit')
+      }
+      else {
+          console.log('octokit successful')
+      }
   }
-  else{
-      console.log('octokit successful')
-  }
-}
